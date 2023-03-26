1feab62976ea0a3cfb0b8a9099d87f64bc56c145
iritmaximus <aaro.koivukangas@gmail.com>
2023-03-26 23:51:59 +0300

    removed bin and updated nvim packer.lua

--- nvim/lua/iritmaximus/packer.lua
+++ nvim/lua/iritmaximus/packer.lua
@@ -4,32 +4,31 @@
 vim.cmd [[packadd packer.nvim]]
 
 return require('packer').startup(function(use)
-  -- Packer can manage itself
-  use 'wbthomason/packer.nvim'
-  use {
-	'nvim-telescope/telescope.nvim', tag = '0.1.1',
-	-- or                            , branch = '0.1.x',
-	requires = { {'nvim-lua/plenary.nvim'} }
-  }
-  use({
-	  'rose-pine/neovim',
-	  as = 'rose-pine',
-  })
-  use 'sainnhe/everforest'
+    -- Packer can manage itself
+    use 'wbthomason/packer.nvim'
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.1',
+        -- or                            , branch = '0.1.x',
+        requires = { { 'nvim-lua/plenary.nvim' } }
+    }
+    use({
+        'rose-pine/neovim',
+        as = 'rose-pine',
+    })
+    use 'sainnhe/everforest'
 
-  use('nvim-treesitter/nvim-treesitter', {run = ':TSUpdate'})
-  use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
-  use 'jose-elias-alvarez/null-ls.nvim'
-  use 'airblade/vim-gitgutter'
+    use('nvim-treesitter/nvim-treesitter', { run = ':TSUpdate' })
+    use 'neovim/nvim-lspconfig' -- Configurations for Nvim LSP
+    use 'jose-elias-alvarez/null-ls.nvim'
+    use 'airblade/vim-gitgutter'
 
-  use 'hrsh7th/nvim-cmp'
-  use 'onsails/lspkind.nvim'
-  use 'hrsh7th/cmp-nvim-lsp'
-  use 'hrsh7th/cmp-buffer'
-  use 'hrsh7th/cmp-path'
-  use 'tpope/vim-fugitive'
+    use 'hrsh7th/nvim-cmp'
+    use 'onsails/lspkind.nvim'
+    use 'hrsh7th/cmp-nvim-lsp'
+    use 'hrsh7th/cmp-buffer'
+    use 'hrsh7th/cmp-path'
+    use 'tpope/vim-fugitive'
 
-  use 'nvim-orgmode/orgmode'
-  use 'windwp/nvim-autopairs'
-
+    use 'nvim-orgmode/orgmode'
+    use 'windwp/nvim-autopairs'
 end)
